75df5a3c0fee267e4cfe50cdefe7871f208ecef3
Chris <csnow@live.ca>
2024-05-21 19:16:18 -0400

    Add leetcode url because why not?

--- src/app/components/footer.tsx
+++ src/app/components/footer.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faGithub } from "@fortawesome/free-brands-svg-icons/faGithub";
 import { faLinkedin } from "@fortawesome/free-brands-svg-icons/faLinkedin";
+import { faCode } from "@fortawesome/free-solid-svg-icons/faCode";
 
 export default function Footer() {
   return (
@@ -13,6 +14,9 @@ export default function Footer() {
         </a>
         <a className="linkedin" href="https://linkedin.com/in/christopher-snow-0" target="_blank" rel="noreferrer noopener">
           <FontAwesomeIcon icon={faLinkedin} size="lg" color="black" />
+        </a>
+        <a className="leetcode" href="https://leetcode.com/u/christofosho" target="_blank" rel="noreferrer noopener">
+          <FontAwesomeIcon icon={faCode} size="lg" color="black" />
         </a>
       </nav>
     </footer>
